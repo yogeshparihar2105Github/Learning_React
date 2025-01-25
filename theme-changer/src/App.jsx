6fefc95481c717c6545b64609fc5636116b10cc4
Yogesh Parihar <yogeshparihar2105@gmail.com>
2025-01-25 21:45:32 +0530

    added the project of theme-changer

---
+++ theme-changer/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Theme Changer</h1>
+      <button onClick={() => setCount((count) => count + 1)}>Count: {count}</button>
+      <div>{count}</div>
+    </>
+  )
+}
+
+export default App
